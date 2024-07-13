1315a5ac9498d4423fe5f9080a48c4b2a977e541
rhr18818 <rhr14101@gmail.com>
2024-07-13 23:02:28 +0600

    Uplading some code

---
+++ Day1/variable.js
@@ -0,0 +1,14 @@
+// difference between let and const =>
+    // let is a block scoped variable // const is a block scoped variable  
+    // let is mutable // const is immutable
+    // let is a variable // const is a constant
+    // what is mutable? // mutable means that the value of the variable can be changed
+const studentId = 20222051010
+let studentName = "Ratul Hasan"
+
+//studentId =1; -> it's const can't be changed
+studentName = "Ratul Hasan Rabbi"
+
+// console.log(studentId);
+// console.log(studentName);
+console.table([studentId,studentName]);
